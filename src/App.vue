7073885894764d12c10fd529838d70f75758f050
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-29 12:26:33 +0300

    Project was created.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="todolist">
+
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+        background: #f7f1f1;
+        font-size: 1.1rem;
+        font-family: 'Noto Sans JP', sans-serif;
+        height: 100%;
+  }
+
+  #todolist {
+    margin: 4rem auto;
+  }
 </style>
